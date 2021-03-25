9f3bda5f2e62e924895ddfd900f7400519b21fa7
Josh Shetler <63809330+J-Pirate@users.noreply.github.com>
2021-03-24 20:39:49 -0700

    Added exception handling for the file browser

---
+++ PDFread/src/main/java/PDFread/PDFread/SubmittalUI.java
@@ -0,0 +1,179 @@
+package submittalProj.project;
+import java.awt.Color;
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+import javax.swing.JOptionPane;
+
+import java.awt.Window.Type;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.io.File;
+import javax.swing.JPanel;
+import java.awt.Font;
+import javax.swing.JButton;
+import javax.swing.JFileChooser;
+import javax.swing.GroupLayout;
+import javax.swing.GroupLayout.Alignment;
+import javax.swing.JTextField;
+import javax.swing.LayoutStyle.ComponentPlacement;
+import javax.swing.filechooser.FileNameExtensionFilter;
+import javax.swing.JTextPane;
+import java.awt.SystemColor;
+import javax.swing.JProgressBar;
+import java.awt.FlowLayout;
+
+public class SubmittalUI {
+
+	private JFrame frmSubmittalXpress;
+	private JTextField textField;
+	private JButton btnNewButton;
+	protected static String filePath; 
+	private JFileChooser jFilePick;
+	private JFrame pop;
+	
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					SubmittalUI window = new SubmittalUI();
+					window.frmSubmittalXpress.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+	
+	
+	public void openFile() {
+//initialize jFilePicker		
+		boolean isPdf = true;
+		jFilePick = new JFileChooser();
+//set a new file filter matching pdf extension
+		jFilePick.resetChoosableFileFilters();
+		jFilePick.setFileFilter(new FileNameExtensionFilter("*.pdf", "pdf","PDF","Pdf"));
+		jFilePick.setAcceptAllFileFilterUsed(false);
+		
+		while(isPdf) {
+		int j = jFilePick.showOpenDialog(frmSubmittalXpress);
+		File takeFile = jFilePick.getSelectedFile();
+		if(takeFile != null) {
+		filePath = takeFile.getAbsolutePath();
+		}
+//if the integer shows 1, and is the same as approve option,
+// then take file path. 
+		 if (JFileChooser.APPROVE_OPTION == j && filePath.endsWith(".pdf")) {
+			   textField.setText(filePath);	 
+		} else {
+			JOptionPane.showMessageDialog(pop, "Invalid, not a .pdf extension", "ALERT", JOptionPane.WARNING_MESSAGE);
+		}
+		 isPdf = false; 
+	}
+}
+	
+	/**
+	 * Create the application.
+	 */
+	public SubmittalUI() {
+		initialize();
+	}
+
+	/**
+	 * Initialize the contents of the frame.
+	 */
+	private void initialize() {
+		frmSubmittalXpress = new JFrame();
+		frmSubmittalXpress.getContentPane().setBackground(Color.LIGHT_GRAY);
+		frmSubmittalXpress.getContentPane().setEnabled(false);
+		frmSubmittalXpress.setFont(new Font("MS PGothic", Font.BOLD, 16));
+		frmSubmittalXpress.setTitle("SubmittalXPress");
+		frmSubmittalXpress.setType(Type.NORMAL);
+		frmSubmittalXpress.getContentPane().setLayout(new FlowLayout(FlowLayout.CENTER, 5, 5));
+		
+		JPanel panel = new JPanel();
+		panel.setBackground(Color.LIGHT_GRAY);
+		frmSubmittalXpress.getContentPane().add(panel);
+		
+	    btnNewButton = new JButton("Browse File...");
+		btnNewButton.addActionListener(actionListener);
+		
+		textField = new JTextField();
+		textField.setColumns(10);
+		textField.setAutoscrolls(true);
+		textField.setEditable(false);
+		
+		JTextPane txtpnSubmittalxpress = new JTextPane();
+		txtpnSubmittalxpress.setEditable(false);
+		txtpnSubmittalxpress.setBackground(SystemColor.menu);
+		txtpnSubmittalxpress.setText("               SubmittalXPress");
+		txtpnSubmittalxpress.setFont(new Font("Rockwell", Font.PLAIN, 18));
+		
+		JProgressBar progressBar = new JProgressBar();
+		GroupLayout gl_panel = new GroupLayout(panel);
+		gl_panel.setHorizontalGroup(
+			gl_panel.createParallelGroup(Alignment.LEADING)
+				.addGroup(gl_panel.createSequentialGroup()
+					.addGroup(gl_panel.createParallelGroup(Alignment.LEADING)
+						.addGroup(gl_panel.createSequentialGroup()
+							.addGap(165)
+							.addComponent(textField, GroupLayout.PREFERRED_SIZE, 248, GroupLayout.PREFERRED_SIZE))
+						.addGroup(gl_panel.createSequentialGroup()
+							.addGap(242)
+							.addComponent(btnNewButton))
+						.addGroup(gl_panel.createSequentialGroup()
+							.addGap(139)
+							.addComponent(txtpnSubmittalxpress, GroupLayout.PREFERRED_SIZE, 304, GroupLayout.PREFERRED_SIZE))
+						.addGroup(gl_panel.createSequentialGroup()
+							.addGap(204)
+							.addComponent(progressBar, GroupLayout.PREFERRED_SIZE, 175, GroupLayout.PREFERRED_SIZE)))
+					.addContainerGap(174, Short.MAX_VALUE))
+		);
+		gl_panel.setVerticalGroup(
+			gl_panel.createParallelGroup(Alignment.LEADING)
+				.addGroup(gl_panel.createSequentialGroup()
+					.addGap(50)
+					.addComponent(txtpnSubmittalxpress, GroupLayout.PREFERRED_SIZE, 41, GroupLayout.PREFERRED_SIZE)
+					.addPreferredGap(ComponentPlacement.UNRELATED)
+					.addComponent(btnNewButton)
+					.addPreferredGap(ComponentPlacement.UNRELATED)
+					.addComponent(textField, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE)
+					.addGap(40)
+					.addComponent(progressBar, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE)
+					.addContainerGap(261, Short.MAX_VALUE))
+		);
+		panel.setLayout(gl_panel);
+		frmSubmittalXpress.setBounds(500, 140, 631, 500);
+		frmSubmittalXpress.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+	}
+	
+	 ActionListener actionListener = new ActionListener() {
+	        @Override
+	        public void actionPerformed(ActionEvent a) {
+	        
+	        if(a.getSource() == btnNewButton) {
+	        	
+	        	openFile();
+	        	
+	        //	try {
+				//	ReadandWritePDF.readPDFTest();
+				//} catch (IOException e) {
+					// TODO Auto-generated catch block
+				//	e.printStackTrace();
+				//}
+	        	
+	        	}
+	       
+	        }
+	   
+	     };
+
+
+}
+	
+	
+	
+	
