e438c4dc30f51b28fcaf21020eca14bec7c6668b
Joshua Shetler <jshetler23@my.whitworth.edu>
2021-04-20 14:12:48 -0700

    Finished with new continue button, Started working on database screen

--- PDFread/src/main/java/PDFread/PDFread/SubmittalUI.java
+++ PDFread/src/main/java/PDFread/PDFread/SubmittalUI.java
@@ -33,7 +33,7 @@ import javax.swing.JProgressBar;
 public class SubmittalUI {
 	
 	private JFrame frame;
-	private JFrame f = new JFrame("Second");
+	private JFrame f;
 	private static JTextArea textArea;
 	///////////////////////
 	private JPanel checklistpanel;
@@ -47,7 +47,9 @@ public class SubmittalUI {
 	protected static SubmittalUI window = new SubmittalUI();
 	public PDFreader PDFtest;
 	private JProgressBar progressBar; 
-	
+	private JButton Continue2;
+	private JFrame dataScrn;
+	private JPanel scrn2;
 	 ArrayList<JCheckBox> CheckboxList = new ArrayList<JCheckBox>();
 	
 	/**
@@ -101,7 +103,7 @@ public class SubmittalUI {
 	public SubmittalUI() {
 		initialize();
 	}
-
+	
 	/**
 	 * Initialize the contents of the frame.
 	 */
@@ -180,8 +182,149 @@ public class SubmittalUI {
 		frmSubmittalXpress.setBounds(500, 140, 631, 500);
 		frmSubmittalXpress.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 	}
-	
-	 ActionListener actionListener = new ActionListener() {
+	             
+	        	public void SecScreen() {
+	        		
+	        		checklistpanel = new JPanel();
+	        		f = new JFrame();
+	        		f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+	        		f.setSize(660,500);
+	        		f.getContentPane().setLayout(null);
+	        		f.setLocationRelativeTo(null);
+	        		
+	        	    scrn2 = new JPanel();
+	        		scrn2.setBounds(10, 10, 631, 453);
+	        		f.getContentPane().add(scrn2);
+	        		
+	        		JLabel lblNewLabel = new JLabel("Specification Sections");
+	        		
+	        	    textArea = new JTextArea("");
+	        	    Font newFont = new Font("SansSerif", Font.PLAIN, 14);
+	        	    textArea.setFont(newFont);
+	        	    appendSpec();
+	        	 
+	        		JLabel lblNewLabel_1 = new JLabel("Select Sections");
+	        		
+	        	    Continue2 = new JButton("Continue");
+	        		Continue2.addActionListener(actionListener);
+	        		
+	        		GroupLayout gl_panel = new GroupLayout(scrn2);
+	        		gl_panel.setHorizontalGroup(
+	        			gl_panel.createParallelGroup(Alignment.TRAILING)
+	        				.addGroup(gl_panel.createSequentialGroup()
+	        					.addGap(74)
+	        					.addComponent(lblNewLabel_1, GroupLayout.PREFERRED_SIZE, 108, GroupLayout.PREFERRED_SIZE)
+	        					.addPreferredGap(ComponentPlacement.RELATED, 49, Short.MAX_VALUE)
+	        					.addComponent(lblNewLabel, GroupLayout.PREFERRED_SIZE, 150, GroupLayout.PREFERRED_SIZE)
+	        					.addGap(250))
+	        				.addGroup(Alignment.LEADING, gl_panel.createSequentialGroup()
+	        					.addGap(39)
+	        					.addComponent(checklistpanel, GroupLayout.PREFERRED_SIZE, 60, GroupLayout.PREFERRED_SIZE)
+	        					.addPreferredGap(ComponentPlacement.UNRELATED)
+	        					.addComponent(textArea, GroupLayout.PREFERRED_SIZE, 257, GroupLayout.PREFERRED_SIZE)
+	        					.addGap(88)
+	        					.addComponent(Continue2)
+	        					.addContainerGap(92, Short.MAX_VALUE))
+	        		);
+	        		gl_panel.setVerticalGroup(
+	        			gl_panel.createParallelGroup(Alignment.LEADING)
+	        				.addGroup(gl_panel.createSequentialGroup()
+	        					.addGroup(gl_panel.createParallelGroup(Alignment.LEADING)
+	        						.addGroup(gl_panel.createSequentialGroup()
+	        							.addGap(48)
+	        							.addGroup(gl_panel.createParallelGroup(Alignment.BASELINE)
+	        								.addComponent(lblNewLabel)
+	        								.addComponent(lblNewLabel_1))
+	        							.addPreferredGap(ComponentPlacement.RELATED)
+	        							.addGroup(gl_panel.createParallelGroup(Alignment.TRAILING, false)
+	        								.addComponent(checklistpanel, Alignment.LEADING, GroupLayout.DEFAULT_SIZE, GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+	        								.addComponent(textArea, Alignment.LEADING, GroupLayout.DEFAULT_SIZE, 305, Short.MAX_VALUE)))
+	        						.addGroup(gl_panel.createSequentialGroup()
+	        							.addGap(208)
+	        							.addComponent(Continue2)))
+	        					.addContainerGap(81, Short.MAX_VALUE))
+	        		);
+	        		checklistpanel.setLayout(new FlowLayout(FlowLayout.CENTER, 5, 5));
+	        		
+	        		scrn2.setLayout(gl_panel);
+	        		f.setVisible(true);
+	        	}
+
+	        public void appendSpec() {
+	        	
+	        	 for (int i = 0; i < PDFtest.D22Spec.size(); i++) {
+	        	    	textArea.append(PDFtest.D22Spec.elementAt(i));
+	        	    	textArea.append("\n");		
+	        	    	textArea.append("\n");
+	        	    	JCheckBox check = new JCheckBox("Keep");
+	        	    	check.addActionListener(checkbox);
+	        	    	CheckboxList.add(check);
+		        		checklistpanel.add(check);
+		        		
+	        }
+	      }
+	        
+	     
+	        //TODO CheckBox action listener
+	        ActionListener checkbox = new ActionListener() {
+	        
+	        public void actionPerformed(ActionEvent e) {
+	        	
+	            for (JCheckBox checkBox : CheckboxList) {
+	                if (checkBox.isSelected()) {
+	                  //TODO something
+	                }
+	            }
+	            //TODO something
+	        }
+	        
+	  };
+	        
+	               
+	  public void DataBaseScreen() {
+		  
+		    dataScrn = new JFrame();
+		    dataScrn.setBounds(10, 10, 631, 453);
+		    dataScrn.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		    dataScrn.setVisible(true);
+			
+			JButton next = new JButton("Continue");
+			
+			JTextPane textPane = new JTextPane();
+			
+			JLabel lblNewLabel = new JLabel("Database Info");
+			GroupLayout groupLayout = new GroupLayout(dataScrn.getContentPane());
+			groupLayout.setHorizontalGroup(
+				groupLayout.createParallelGroup(Alignment.LEADING)
+					.addGroup(groupLayout.createSequentialGroup()
+						.addContainerGap(84, Short.MAX_VALUE)
+						.addGroup(groupLayout.createParallelGroup(Alignment.LEADING)
+							.addGroup(Alignment.TRAILING, groupLayout.createSequentialGroup()
+								.addComponent(next)
+								.addContainerGap())
+							.addGroup(Alignment.TRAILING, groupLayout.createSequentialGroup()
+								.addComponent(textPane, GroupLayout.PREFERRED_SIZE, 275, GroupLayout.PREFERRED_SIZE)
+								.addGap(77))
+							.addGroup(Alignment.TRAILING, groupLayout.createSequentialGroup()
+								.addComponent(lblNewLabel, GroupLayout.PREFERRED_SIZE, 96, GroupLayout.PREFERRED_SIZE)
+								.addGap(165))))
+			);
+			groupLayout.setVerticalGroup(
+				groupLayout.createParallelGroup(Alignment.LEADING)
+					.addGroup(Alignment.TRAILING, groupLayout.createSequentialGroup()
+						.addGap(29)
+						.addComponent(lblNewLabel)
+						.addPreferredGap(ComponentPlacement.UNRELATED)
+						.addComponent(textPane, GroupLayout.PREFERRED_SIZE, 98, GroupLayout.PREFERRED_SIZE)
+						.addPreferredGap(ComponentPlacement.RELATED, 82, Short.MAX_VALUE)
+						.addComponent(next)
+						.addContainerGap())
+			);
+			dataScrn.getContentPane().setLayout(groupLayout);
+		}
+	  
+	  
+	  ActionListener actionListener = new ActionListener() {
 	        @Override
 	        
 	        public void actionPerformed(ActionEvent a) {
@@ -189,14 +332,12 @@ public class SubmittalUI {
 	        	progressBar.setIndeterminate(true);
 				
 	        	
-	        if(a.getSource() == btnNewButton) {
+			if(a.getSource() == btnNewButton) {
 	        	
 	        	openFile();
 	        	
 	        }else if(a.getSource() == Continue && filePath != null) {
 	        	
-	  
-
 					try {
 						
 						System.out.print("Starting PDF reader \n");
@@ -207,7 +348,7 @@ public class SubmittalUI {
 				        PDFtest = new PDFreader(filePath);
 				        PDFtest.checkSpecs();
 		//		        System.out.print("Here are the specification sections we found in Division 22: \n");
-       //				        PDFtest.printSpecs();
+     //				        PDFtest.printSpecs();
 				        System.out.print("================================================");
 				        System.out.print("\nHere are the corresponding page numbers: \n");
 				        PDFtest.printPages();
@@ -232,117 +373,19 @@ public class SubmittalUI {
 						e.printStackTrace();
 					}
 			      
+	        } else if(a.getSource() == Continue2) {
+	        	System.out.print("Im working");
+	        	window.f.setVisible(false);
+	        	window.f.dispose();
+	        	DataBaseScreen();
 	        }
 	        	
 	        	}
 	        };
+	  
 	        
-	       // public void SecScreen  {
-	        
-	        	public void SecScreen() {
-	        		
-	        		checklistpanel = new JPanel();
-	        		
-	        		f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-	        		f.setSize(660,500);
-	        		f.getContentPane().setLayout(null);
-	        		f.setLocationRelativeTo(null);
-	        		
-	        		JPanel panel = new JPanel();
-	        		panel.setBounds(10, 10, 631, 453);
-	        		f.getContentPane().add(panel);
-	        		
-	        		JLabel lblNewLabel = new JLabel("Specification Sections");
-	        		
-	        	    textArea = new JTextArea("");
-	        	    Font newFont = new Font("SansSerif", Font.PLAIN, 14);
-	        	    textArea.setFont(newFont);
-	        	    appendSpec();
-	        	 
-	        		JLabel lblNewLabel_1 = new JLabel("Select Sections");
-	        		GroupLayout gl_panel = new GroupLayout(panel);
-	        		gl_panel.setHorizontalGroup(
-	        			gl_panel.createParallelGroup(Alignment.LEADING)
-	        				.addGroup(Alignment.TRAILING, gl_panel.createSequentialGroup()
-	        					.addGap(74)
-	        					.addComponent(lblNewLabel_1, GroupLayout.PREFERRED_SIZE, 108, GroupLayout.PREFERRED_SIZE)
-	        					.addPreferredGap(ComponentPlacement.RELATED, 113, Short.MAX_VALUE)
-	        					.addComponent(lblNewLabel, GroupLayout.PREFERRED_SIZE, 150, GroupLayout.PREFERRED_SIZE)
-	        					.addGap(250))
-	        				.addGroup(gl_panel.createSequentialGroup()
-	        					.addGap(39)
-	        					.addComponent(checklistpanel, GroupLayout.PREFERRED_SIZE, 60, GroupLayout.PREFERRED_SIZE)
-	        					.addPreferredGap(ComponentPlacement.UNRELATED)
-	        					.addComponent(textArea, GroupLayout.PREFERRED_SIZE, 257, GroupLayout.PREFERRED_SIZE)
-	        					.addContainerGap(190, Short.MAX_VALUE))
-	        		);
-	        		gl_panel.setVerticalGroup(
-	        			gl_panel.createParallelGroup(Alignment.LEADING)
-	        				.addGroup(gl_panel.createSequentialGroup()
-	        					.addGap(48)
-	        					.addGroup(gl_panel.createParallelGroup(Alignment.BASELINE)
-	        						.addComponent(lblNewLabel)
-	        						.addComponent(lblNewLabel_1))
-	        					.addPreferredGap(ComponentPlacement.RELATED)
-	        					.addGroup(gl_panel.createParallelGroup(Alignment.TRAILING, false)
-	        						.addComponent(checklistpanel, Alignment.LEADING, GroupLayout.DEFAULT_SIZE, GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-	        						.addComponent(textArea, Alignment.LEADING, GroupLayout.DEFAULT_SIZE, 305, Short.MAX_VALUE))
-	        					.addContainerGap(81, Short.MAX_VALUE))
-	        		);
-	        		checklistpanel.setLayout(new FlowLayout(FlowLayout.CENTER, 5, 5));
-	        		
-	        		panel.setLayout(gl_panel);
-	        		f.setVisible(true);
-	        		
-	        	}
-
-	        public void appendSpec() {
-	        	
-	        	 for (int i = 0; i < PDFtest.D22Spec.size(); i++) {
-	        	    	textArea.append(PDFtest.D22Spec.elementAt(i));
-	        	    	textArea.append("\n");		
-	        	    	textArea.append("\n");
-	        	    	JCheckBox check = new JCheckBox("Keep");
-	        	    	check.addActionListener(actionListener);
-	        	    	CheckboxList.add(check);
-		        		checklistpanel.add(check);
-	        }
-	      }
-	        
-	        
-	        ActionListener checkbox = new ActionListener() {
-	        
-	        public void actionPerformed(ActionEvent e) {
-	        	
-	            for (JCheckBox checkBox : CheckboxList) {
-	                if (checkBox.isSelected()) {
-	                  
-	                    
-	                }
-	            }
-	            // TODO do something with infos
-	        }
-	        };
-	        
-	               
-	        
-	        
-	        
+		  
 };
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-	
-	
